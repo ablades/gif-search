a0ecadb681d0e9c64f59ac588e11c36aa4535b58
ablades <audaris@gmail.com>
2019-09-12 16:56:00 -0700

    add button selection

--- app.py
+++ app.py
@@ -4,26 +4,37 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     """Return homepage."""
+
     #Extract query term from url
     search = request.args.get("search")
 
-    #Make 'params' dict with query term and API key
     params = {
-        "q" : search,
-        "key": '4HIPRMNS9U52',
-        "limit": 10}
+    "q" : search,
+    "key": '4HIPRMNS9U52',
+    "limit": 10}
+
+    button_request = request.args.get('button')
+
+    r = requests.get("https://api.tenor.com/v1/search", params)
+    if button_request == "trending":
+        search = None
+        r = requests.get("https://api.tenor.com/v1/trending?", params)
+    elif button_request == "random":
+        search = 'random'
+        r = requests.get("https://api.tenor.com/v1/random?", params)
 
     #Make an API call to Tenor using the 'requests' library
-    r = requests.get("https://api.tenor.com/v1/search", params)
+    # r = requests.get("https://api.tenor.com/v1/search", params)
+    #Get post request
 
     gifs = json.loads(r.content)['results']
-
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
     return render_template(
         "index.html", gifs=gifs)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
